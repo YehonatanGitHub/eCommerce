4aeecf6ab397275fa84197894dc9e3fb47591c56
Jonathan R <2140173@gmail.com>
2019-12-19 13:09:22 +0200

    updat

--- src/app/shared/data.service.ts
+++ src/app/shared/data.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@angular/core';
-import { }
-
+import { HttpClient } from '@angular/common/http';
+// import { postData } from '../shopping/sidebar/admin/admin.component';
 @Injectable({ providedIn: 'root' })
 export class DataService {
-    constructor()
+    constructor(private http: HttpClient) {
+    }
+
+    // storeNewProduct(product: postData[]) {
+
+    // }
 }
--- src/app/shopping/sidebar/admin/admin.component.ts
+++ src/app/shopping/sidebar/admin/admin.component.ts
@@ -1,16 +1,17 @@
 import { Component, OnInit } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { HttpClient } from "@angular/common/http";
-
+import { DataService } from "../../../shared/data.service"
 @Component({
   selector: "app-admin",
   templateUrl: "./admin.component.html",
   styleUrls: ["./admin.component.css"]
+
 })
 export class AdminComponent implements OnInit {
   private _opened: boolean = false;
   newProduct: '';
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private dataService: DataService) { }
 
   ngOnInit() { }
   private _toggleAddProduct() {
