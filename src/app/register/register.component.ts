9bd3279b807e8f9d7e975918e0af5375b7079797
Jonathan R <2140173@gmail.com>
2020-01-06 12:50:55 +0200

    Sign-up ID valadation to DB

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { Register1 } from '../../app/register/register1.model'
 import { HttpClient } from "@angular/common/http";
+import { responseData } from '../../app/register/responseData.model'
 import { FormGroup, FormControl, Validators, ValidatorFn, AbstractControl } from '@angular/forms';
 import { Observable } from 'rxjs';
 
@@ -12,9 +13,9 @@ import { Observable } from 'rxjs';
 export class RegisterComponent implements OnInit {
   signupForm: FormGroup;
   passwordCheck = ''; // Confirm with password
-  tzCheckDb = {
-    tz: Number
-  };
+  // responseData = {
+  //   result: Boolean
+  // };
   userExists = false;
 
   constructor(private http: HttpClient) { }
@@ -22,7 +23,8 @@ export class RegisterComponent implements OnInit {
   ngOnInit() {
     this.signupForm = new FormGroup({
       'firstStep': new FormGroup({
-        'tz': new FormControl(null, [Validators.required, Validators.min(10000000), Validators.max(999999999)], this.checkIfUserExists.bind(this)),
+
+        'tz': new FormControl(null, [Validators.required, Validators.pattern(/^(?!0{2})[0-9]{9}$/)], this.checkIfUserExists.bind(this)),
         'email': new FormControl('', [Validators.required,
         Validators.pattern(/^[_a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/)]),
         'password': new FormControl(null, [Validators.required, Validators.minLength(6)]),
@@ -50,18 +52,23 @@ export class RegisterComponent implements OnInit {
 
 
   checkIfUserExists(control: FormControl): Promise<any> | Observable<any> {
-    this.tzCheckDb = { tz: control.value };
+    const tester = { tz: control.value }
+    console.log(tester);
+
     const promise = new Promise<any>((resolve, reject) => {
-      this.http.post('http://localhost:3000/users/checkifuser', this.tzCheckDb)
-        .subscribe(responseData => {
-          console.log(responseData);
-          if (responseData == true) {
+      this.http.post('http://localhost:3000/users/checkifuser', tester)
+        .subscribe((responseData: responseData) => {
+          console.log(responseData.result);
+
+          if (responseData.result === true) {
+            console.log('userExists true');
             resolve({ 'userExists': true });
           } else {
+            console.log('null');
             resolve(null);
           }
 
-        });
+        })
     });
     return promise;
   }
